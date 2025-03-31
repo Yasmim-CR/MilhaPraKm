a93657521ec8695d44348fe093c31e38efdee9a0
Yasmim de Castro <yas.castr0.r@gmail.com>
2025-03-31 08:17:28 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Escreva um valor em milhas: ");
+
+    double milha = Convert.ToDouble(Console.ReadLine()!);
+    double km = milha * 1.609;
+Console.Write($"Seu valor em quilômetros é {km}");
